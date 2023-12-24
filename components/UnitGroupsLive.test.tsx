9bd047614040c11a9cae060edefe3be963cf484e
Ben Watts <ben@kilowatts.io>
2023-12-24 11:24:45 +0000

    add unit tests for searching in the unit groups screen

---
+++ components/UnitGroupsLive.test.tsx
@@ -0,0 +1,89 @@
+// test rendering the UnitGroupsLive component, and entering text into the search box
+import { render, screen, fireEvent } from "@testing-library/react-native";
+import { UnitGroupsLive } from "./UnitGroupsLive";
+
+const mockRenders = [
+  jest.spyOn(require("../components/UnitGroupsLiveList"), "UnitGroupLiveList"),
+  jest.spyOn(require("../atoms/inputs"), "SearchUnitGroups"),
+];
+
+// mock the list component
+jest.mock("../components/UnitGroupsLiveList", () => ({
+  UnitGroupLiveList: () => <div>UnitGroupLiveList</div>,
+}));
+
+const typingEvents = {
+  drax: () => {
+    const input = screen.getByPlaceholderText("Search");
+    fireEvent.changeText(input, "Drax");
+    return input;
+  },
+  ratcliffe: () => {
+    const input = screen.getByPlaceholderText("Search");
+    fireEvent.changeText(input, "Ratcliffe");
+    return input;
+  }
+};
+
+describe("UnitGroupsLive/noFuelType", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    render(<UnitGroupsLive />);
+  });
+
+  test("search input is visible", () => {
+    screen.getByPlaceholderText("Search");
+  });
+
+  test('UnitGroupLiveList is rendered with the prop search == "" and fuelType undefined ', () => {
+    expect(mockRenders[0]).toHaveBeenCalledWith(
+      { search: "", fuelType: undefined },
+      {}
+    );
+  });
+
+  test("can enter Drax into search input and this is reflected in the rendered text", () => {
+    const input = typingEvents.drax();
+    expect(input.props.value).toBe("Drax");
+  });
+
+  test("after typing Drax, UnitGroupLiveList is called with prop Search == Drax", () => {
+    typingEvents.drax();
+    expect(mockRenders[0]).toHaveBeenCalledWith(
+      { search: "Drax", fuelType: undefined },
+      {}
+    );
+  });
+});
+
+
+describe("UnitGroupsLive/coal", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    render(<UnitGroupsLive fuelType="coal"/>);
+  });
+
+  test("search input is visible", () => {
+    screen.getByPlaceholderText("Search");
+  });
+
+  test('UnitGroupLiveList is rendered with the prop search == "" and fuelType coal ', () => {
+    expect(mockRenders[0]).toHaveBeenCalledWith(
+      { search: "", fuelType: 'coal' },
+      {}
+    );
+  });
+
+  test("can enter Ratcliffe into search input and this is reflected in the rendered text", () => {
+    const input = typingEvents.ratcliffe();
+    expect(input.props.value).toBe("Ratcliffe");
+  });
+
+  test("after typing Drax, UnitGroupLiveList is called with prop Search == Ratcliffe", () => {
+    typingEvents.ratcliffe();
+    expect(mockRenders[0]).toHaveBeenCalledWith(
+      { search: "Ratcliffe", fuelType: 'coal' },
+      {}
+    );
+  });
+});
