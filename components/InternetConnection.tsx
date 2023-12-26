3805203419add776df16a9a3ad9dbf67a1fe4f2c
Ben Watts <ben@kilowatts.io>
2023-12-26 09:15:05 +0000

    replace loading screen with one that is complete

--- components/InternetConnection.tsx
+++ components/InternetConnection.tsx
@@ -1,18 +1,18 @@
 import { ExpoRoot } from "expo-router";
 import React from "react";
 import { NoInternetConnectionCard } from "../atoms/cards";
-import { Refresh } from "../atoms/controls";
 import { WithLicense } from "./WithLicense";
 import { useInternetConnection } from "../services/hooks";
 import { ForegroundComponent } from "./Foreground";
+import { BackgroundScreen } from "../atoms/screens";
 
 type InternetConnectionProps = {};
 
 export const InternetConnection: React.FC<InternetConnectionProps> = () => {
-  const { isConnected } = useInternetConnection();
+  const isConnected = useInternetConnection();
   return (
     <>
-      {isConnected === null && <Refresh refreshing={true} />}
+      {isConnected === null && <BackgroundScreen/>}
       {isConnected === false && <NoInternetConnectionCard />}
       {isConnected === true && (
         <WithLicense>
